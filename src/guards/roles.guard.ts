8461680c9811fee2e86ae204985cc905bb35eb25
Gaëtan Boyals <boyalsgnz@gmail.com>
2026-08-12 19:55:37 +0800

    implementing Roles decorator for role-based auth

--- src/guards/roles.guard.ts
+++ src/guards/roles.guard.ts
@@ -1,6 +1,7 @@
 import { Injectable, CanActivate, ExecutionContext } from '@nestjs/common';
 import { Observable } from 'rxjs';
 import { Reflector } from '@nestjs/core';
+import { Roles } from '../decorators/roles.decorator';
 
 @Injectable()
 export class RolesGuard implements CanActivate {
@@ -8,7 +9,13 @@ export class RolesGuard implements CanActivate {
   canActivate(
     context: ExecutionContext,
   ): boolean | Promise<boolean> | Observable<boolean> {
-    console.log(context.getArgs());
+    const roles = this.reflector.get(Roles, context.getHandler());
+    if (!roles) {
+      return true;
+    }
+    const request = context.switchToHttp().getRequest();
+    const user = request.user;
+    console.log(user);
     return true;
   }
 }
